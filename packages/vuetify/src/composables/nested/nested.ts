ff27ea62f2e723a7061163bbf9f5cdd49408f13b
nekosaur <albert@kaaman.nu>
2022-04-30 09:21:14 +0200

    feat(VTreeview): hide checkbox if node not selectable

--- packages/vuetify/src/composables/nested/nested.ts
+++ packages/vuetify/src/composables/nested/nested.ts
@@ -54,6 +54,7 @@ type NestedProvide = {
     selectedClass: Ref<string | undefined>
     getPath: (id: string) => string[]
     getChildren: (id: string) => string[]
+    selectable: (id: string) => boolean
   }
 }
 
@@ -75,6 +76,7 @@ export const emptyNested: NestedProvide = {
     selectedClass: ref(),
     getPath: () => [],
     getChildren: () => [],
+    selectable: () => true,
   },
 }
 
@@ -235,6 +237,11 @@ export const useNested = (props: NestedProps) => {
         })
         newOpened && (opened.value = newOpened)
       },
+      selectable: id => selectStrategy.value.selectable({
+        id,
+        children: children.value,
+        parents: parents.value,
+      }),
       children,
       parents,
       selectedClass: toRef(props, 'selectedClass'),
@@ -265,6 +272,7 @@ export const useNestedItem = (id: Ref<string | undefined>, isGroup: boolean) => 
     parent: computed(() => root.parents.value.get(computedId.value)),
     select: (selected: boolean, e?: Event) => root.select(computedId.value, selected, e),
     isSelected,
+    isSelectable: computed(() => root.selectable(computedId.value)),
     isIndeterminate: computed(() => root.selected.value.get(computedId.value) === 'indeterminate'),
     isLeaf: computed(() => !root.children.value.get(computedId.value)),
     selectedClass: computed(() => isSelected.value && [root.selectedClass.value]),
